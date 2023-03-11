988d7d21c28e2e0e831c301efe550fa70826fb59
kwonlee9876 <75835782+kwonlee9876@users.noreply.github.com>
2023-03-11 21:52:34 +0900

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,56 +15,56 @@ export default function Footer({ showRedirectModal, redirectPopupTimestamp }: Pr
 
   return (
     <div className="Footer">
-    {/* //   <div className={cx("Footer-wrapper", { home: isHome })}>
-    //     <div className="Footer-logo">
-    //       <img src={logoImg} alt="MetaMask" />
-    //     </div>
-    //     <div className="Footer-social-link-block">
-    //       {SOCIAL_LINKS.map((platform) => {
-    //         return (
-    //           <ExternalLink key={platform.name} className="App-social-link" href={platform.link}>
-    //             <img src={platform.icon} alt={platform.name} />
-    //           </ExternalLink>
-    //         );
-    //       })}
-    //     </div>
-    //     <div className="Footer-links">
-    //       {getFooterLinks(isHome).map(({ external, text, link, isAppLink }) => {
-    //         if (external) {
-    //           return (
-    //             <ExternalLink key={i18n._(text)} href={link} className="Footer-link">
-    //               {i18n._(text)}
-    //             </ExternalLink>
-    //           );
-    //         }
-    //         if (isAppLink) {
-    //           if (shouldShowRedirectModal(redirectPopupTimestamp)) {
-    //             return (
-    //               <div
-    //                 key={i18n._(text)}
-    //                 className="Footer-link a"
-    //                 onClick={() => showRedirectModal && showRedirectModal(link)}
-    //               >
-    //                 {i18n._(text)}
-    //               </div>
-    //             );
-    //           } else {
-    //             const baseUrl = getAppBaseUrl();
-    //             return (
-    //               <a key={i18n._(text)} href={baseUrl + link} className="Footer-link">
-    //                 {i18n._(text)}
-    //               </a>
-    //             );
-    //           }
-    //         }
-    //         return (
-    //           <NavLink key={link} to={link} className="Footer-link" activeClassName="active">
-    //             {i18n._(text)}
-    //           </NavLink>
-    //         );
-    //       })}
-    //     </div>
-    //   </div> */}
+       <div className={cx("Footer-wrapper", { home: isHome })}>
+         <div className="Footer-logo">
+           <img src={logoImg} alt="MetaMask" />
+         </div>
+         <div className="Footer-social-link-block">
+           {SOCIAL_LINKS.map((platform) => {
+             return (
+               <ExternalLink key={platform.name} className="App-social-link" href={platform.link}>
+                 <img src={platform.icon} alt={platform.name} />
+               </ExternalLink>
+             );
+           })}
+         </div>
+         <div className="Footer-links">
+           {getFooterLinks(isHome).map(({ external, text, link, isAppLink }) => {
+             if (external) {
+               return (
+                 <ExternalLink key={i18n._(text)} href={link} className="Footer-link">
+                   {i18n._(text)}
+                 </ExternalLink>
+               );
+             }
+             if (isAppLink) {
+               if (shouldShowRedirectModal(redirectPopupTimestamp)) {
+                 return (
+                   <div
+                     key={i18n._(text)}
+                     className="Footer-link a"
+                     onClick={() => showRedirectModal && showRedirectModal(link)}
+                   >
+                     {i18n._(text)}
+                   </div>
+                 );
+               } else {
+                 const baseUrl = getAppBaseUrl();
+                 return (
+                   <a key={i18n._(text)} href={baseUrl + link} className="Footer-link">
+                     {i18n._(text)}
+                   </a>
+                 );
+               }
+             }
+             return (
+               <NavLink key={link} to={link} className="Footer-link" activeClassName="active">
+                 {i18n._(text)}
+               </NavLink>
+             );
+           })}
+         </div>
+       </div> 
     </div>
   );
 }
