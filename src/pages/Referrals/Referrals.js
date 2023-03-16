d7d64965d3139017aac04e1d8aee683a219148ca
kwonlee9876 <75835782+kwonlee9876@users.noreply.github.com>
2023-03-16 17:32:43 +0900

    Update Referrals.js

--- src/pages/Referrals/Referrals.js
+++ src/pages/Referrals/Referrals.js
@@ -132,7 +132,7 @@ function Referrals({ connectWallet, setPendingTxns, pendingTxns }) {
                 IFO sale (10%): 10,000,000 $ZKMX<br />
                 Hard cap : $3,000,000<br /><br />
                 Purchasing currencies: ETH (Arbitrum)<br /><br />
-                  -----------------------------------------------------------------------------------------------
+                  ------------------------------------------------------------------------
                 Price at the 1st round during Auction: $0.15 (Fixed price)<br /><br /> [*after reaching $1.5M cap, then goes to 2nd round]<br />
                 Price at 2st round during Auction: from $0.15 upto $0.3<br /><br />
                 For more information, please read the{" "}<br />
